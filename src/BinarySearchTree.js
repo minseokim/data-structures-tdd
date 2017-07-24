838c402200194753a794155fa9286182b4a65902
minseokim <minseoalexkim@gmail.com>
2017-07-24 17:47:02 -0400

    Finish all test cases for binary search trees

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -4,14 +4,140 @@ const BinarySearchTree = function(data) {
   this.rightChild = null;
 };
 
-BinarySearchTree.prototype.search = function() {};
+BinarySearchTree.prototype.search = function(val) {
+  if (this.data === val) {
+    return true;
+  } else if (this.data > val) {
+    return !!this.leftChild && this.leftChild.search(val);
+  } else if (this.data < val) {
+    return !!this.rightChild && this.rightChild.search(val);
+  }
+  return false;
+};
 
-BinarySearchTree.prototype.traverse = function() {};
+BinarySearchTree.prototype.traverseIn = function(cb) {
+  if (this.leftChild) {
+    this.leftChild.traverseIn(cb);
+  }
 
-BinarySearchTree.prototype.insert = function() {};
+  cb(this);
 
-BinarySearchTree.prototype.delete = function() {};
+  if (this.rightChild) {
+    this.rightChild.traverseIn(cb);
+  }
+};
 
-BinarySearchTree.prototype.findMin = function() {};
+BinarySearchTree.prototype.insert = function(val) {
+  if (this.data > val) {
+    if (this.leftChild) {
+      this.leftChild.insert(val);
+    } else {
+      this.leftChild = new BinarySearchTree(val);
+    }
+  } else if (this.data < val) {
+    if (this.rightChild) {
+      this.rightChild.insert(val);
+    } else {
+      this.rightChild = new BinarySearchTree(val);
+    }
+  } else if (this.data === val) {
+    throw new Error("Node with this value already exists!");
+  }
+};
 
-BinarySearchTree.prototype.findMax = function() {};
+//Pass the parent as an argument!
+BinarySearchTree.prototype.delete = function(val, root) {
+  //we will have to re-set 'this'(which isn't possible), so we will pass in an additional parameter called root that will be initialized to 'this'
+  root = root || this;
+
+  if (!root) return null;
+
+  //Go left
+  if (root.data > val) {
+    root.leftChild = root.delete(val, root.leftChild);
+
+    //Go right
+  } else if (root.data < val) {
+    root.rightChild = root.delete(val, root.rightChild);
+  } else {
+    //We've found our node, need to delete it
+
+    //Caes 1 : Leaf node
+    if (root.leftChild === null && root.rightChild === null) {
+      root = null;
+    } else if (root.leftChild === null) {
+      //Case 2 : Only rightChild or Only leftChild
+      root = root.rightChild;
+    } else if (root.rightChild === null) {
+      root = root.leftChild;
+    } else {
+      //Case 3 : Two children
+
+      //Find minimum in rightSubtree
+      let minInRightSubTree = root.rightChild.findMin();
+      console.log("minInRightSubTree :", minInRightSubTree);
+      //Copy this data into root!
+      root.data = minInRightSubTree.data;
+
+      //now there is a duplicate node in RightSubtree, delete it(We're calling it FROM the reference of the right subtree.)
+      root.rightChild = root.delete(minInRightSubTree.data, root.rightChild);
+    }
+  }
+
+  //Don't forget to return root
+  return root;
+};
+
+BinarySearchTree.prototype.findMin = function() {
+  //Recursive Approach
+  // if (this.leftChild === null) {
+  //   return this;
+  // } else {
+  //   return this.leftChild.findMin();
+  // }
+
+  //Iterative Approach
+  let min = this;
+  while (min.leftChild !== null) {
+    min = min.leftChild;
+  }
+  return min;
+};
+
+BinarySearchTree.prototype.findMax = function() {
+  //Recursive Approach
+  if (this.rightChild === null) {
+    return this;
+  } else {
+    return this.rightChild.findMax();
+  }
+
+  //Iterative Approach
+  // let max = this;
+  // while (max.rightChild !== null) {
+  //   max = max.rightChild;
+  // }
+  // return max;
+};
+
+BinarySearchTree.prototype.deleteMin = function() {
+  //if we've reached leftmost node, return its rightChild
+  //its rightChild can be either an element, or null
+  if (this.leftChild === null) {
+    return this.rightChild;
+  }
+  this.leftChild = this.leftChild.deleteMin();
+  //need to return this every time so the parent can preserve the leftChild
+  return this;
+};
+
+BinarySearchTree.prototype.deleteMax = function() {
+  //if we've reached rightMost node, return its leftChild
+  //its rightChild can be either an element, or null
+  if (this.rightChild === null) {
+    return this.leftChild;
+  }
+  this.rightChild = this.rightChild.deleteMax();
+  //need to return this every time so the parent can preserve the rightChild
+  return this;
+};
--- testSpec/BinarySearchTreeSpec.js
+++ testSpec/BinarySearchTreeSpec.js
@@ -13,18 +13,20 @@ describe("Binary Search Tree", () => {
     assert.equal(BST.hasOwnProperty("rightChild"), true);
   });
 
-  it("should have methods named 'search', 'traverse', 'insert', 'delete', 'findMin', 'findMax'", () => {
+  it("should have methods named 'search', 'traverseIn','insert', 'delete', 'findMin', 'findMax', 'deleteMin', 'deleteMax'", () => {
     assert.typeOf(BST.search, "function");
-    assert.typeOf(BST.traverse, "function");
+    assert.typeOf(BST.traverseIn, "function");
     assert.typeOf(BST.insert, "function");
     assert.typeOf(BST.delete, "function");
     assert.typeOf(BST.findMin, "function");
     assert.typeOf(BST.findMax, "function");
+    assert.typeOf(BST.deleteMin, "function");
+    assert.typeOf(BST.deleteMax, "function");
   });
 
   it("should support inserting into leftChild, and rightChild", () => {
-    assert.equal(BST.leftChild.data, null);
-    assert.equal(BST.rightChild.data, null);
+    assert.equal(BST.leftChild, null);
+    assert.equal(BST.rightChild, null);
     BST.insert(4);
     BST.insert(2);
     assert.equal(BST.leftChild.data, 2);
@@ -50,9 +52,9 @@ describe("Binary Search Tree", () => {
   it("should find the maximum", () => {
     BST.insert(1);
     BST.insert(2);
-    assert.equal(BST.findMin().data, 3);
+    assert.equal(BST.findMax().data, 3);
     BST.insert(7);
-    assert.equal(BST.findMin().data, 7);
+    assert.equal(BST.findMax().data, 7);
   });
 
   it("should traverse in-order", () => {
@@ -64,21 +66,63 @@ describe("Binary Search Tree", () => {
     BST.insert(2);
     BST.insert(6);
     BST.insert(5);
-    BST.traverse(store);
+    BST.traverseIn(store);
     let str = "";
-    store.forEach(curr => {
-      str += String(curr);
-    });
-    assert.equal(str, "23546");
+
+    for (let i = 0; i < result.length; i++) {
+      str += result[i];
+    }
+    assert.equal(str, "23456");
+  });
+
+  it("should support deleteMin", () => {
+    BST.insert(-10);
+    BST.insert(-29);
+    BST.insert(-16);
+    BST.deleteMin();
+    assert.equal(BST.search(-29), false);
+
+    BST.deleteMin();
+    assert.equal(BST.search(-16), false);
+
+    BST.deleteMin();
+    assert.equal(BST.search(-10), false);
+    //Edge case of deleting root - won't delete root itself, will just return null.
+    assert.equal(BST.deleteMin(), null);
+  });
+
+  it("should support deleteMax", () => {
+    BST.insert(5);
+    BST.insert(7);
+    BST.insert(10);
+    BST.deleteMax();
+    assert.equal(BST.search(10), false);
+
+    BST.deleteMax();
+    assert.equal(BST.search(7), false);
+
+    BST.deleteMax();
+    assert.equal(BST.search(5), false);
+    //Edge case of deleting root - won't delete root itself, will just return null.
+    assert.equal(BST.deleteMax(), null);
   });
 
   it("should support delete", () => {
     BST.insert(8);
-    assert.equal(BST.search(8), true);
+    BST.insert(5);
+    BST.insert(10);
+    BST.insert(9);
+    BST.insert(11);
+    //Delete leaf node
+    BST.delete(11);
+    assert.equal(BST.search(11), false);
+
+    //Delete node with one child
+    BST.delete(10);
+    assert.equal(BST.search(10), false);
+
+    //delete node with two children
     BST.delete(8);
     assert.equal(BST.search(8), false);
-    //delete root
-    BST.delete(3);
-    assert.equal(BST.search(3), false);
   });
 });
